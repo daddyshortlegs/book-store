61e2f0b412c599df8e4cbd0b46abdb0486b0f3d3
andys <andysmith836@gmail.com>
2019-04-15 21:46:26 +0100

    Change spring test case to make sure home page is vended

--- src/test/java/com/andy/books/BookStoreApplicationTests.java
+++ src/test/java/com/andy/books/BookStoreApplicationTests.java
@@ -2,15 +2,25 @@ package com.andy.books;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookStoreApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void serverStarts_andLandingPageIsMyBookStore() {
+		String body = this.restTemplate.getForObject("/", String.class);
+
+		assertTrue(body.contains("Andy's Book Store"));
 	}
 
 }
